a3a79ce62e5fa9fdeaa40171976305dd6d451cc0
Manvith Gayam <manvith.gayam@jncc.gov.uk>
2024-08-07 10:06:51 +0100

    Change luigi parameter description

--- container/app/workflow/processing/GenerateS2Indices.py
+++ container/app/workflow/processing/GenerateS2Indices.py
@@ -22,7 +22,7 @@ class GenerateS2Index(luigi.Task):
     stateFolder = luigi.Parameter(default=defaults.Paths["state"])
     index = luigi.parameter.EnumListParameter(
         enum=defaults.S2Indices,
-        description="A comma separted list of any of Brightness,EVI,GLI,GNDVI,GRVI,NBR,NDMI,NDVI,NDWI,RB,RDVI,RG,SAVI,SBL")
+        description="One of Brightness,EVI,GLI,GNDVI,GRVI,NBR,NDMI,NDVI,NDWI,RB,RDVI,RG,SAVI,SBL")
     rBand = luigi.IntParameter(default=defaults.S2IndexDefaults["rBand"])
     gBand = luigi.IntParameter(default=defaults.S2IndexDefaults["gBand"])
     bBand = luigi.IntParameter(default=defaults.S2IndexDefaults["bBand"])
