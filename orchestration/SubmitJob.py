3ff10b86bdf25303ecc595985062fc94a055e4e9
Manvith Gayam <manvith.gayam@jncc.gov.uk>
2024-09-09 11:56:06 +0100

    Split orchestration SubmitJob

--- orchestration/SubmitJob.py
+++ orchestration/SubmitJob.py
@@ -3,15 +3,12 @@ import logging
 import subprocess
 import json
 import os
-from string import Template
 import random
 import re
 import datetime
 
 from luigi import LocalTarget
-from luigi.util import requires
 
-from orchestration.SetupWorkDirs import SetupWorkDirs
 
 log = logging.getLogger('luigi-interface')
 
@@ -66,96 +63,3 @@ class SubmitJob(luigi.Task):
     def output(self):
         outFile = os.path.join(self.stateFolder, f'SubmitJob_{self.name}_{self.jobId}.json')
         return LocalTarget(outFile)
-
-
-class SubmitJobs(luigi.Task):
-    workingFolder = luigi.Parameter()
-    stateFolder = luigi.Parameter()
-    basketFolder = luigi.Parameter()
-    outputFolder = luigi.Parameter()
-    containerPath = luigi.Parameter()
-    indices = luigi.Parameter()
-    templatesDir = luigi.Parameter()
-    testProcessing = luigi.BoolParameter(default=False)
-
-    templateFilename = ""
-    platform = ""
-    sbatchFileName = ""
-    outfile = ""
-
-    def run(self):
-        products = []
-        with self.input().open('r') as setupWorkDirs:
-            products = json.load(setupWorkDirs)["products"]
-
-        with open(os.path.join(self.templatesDir, self.templateFilename), "r") as t:
-            sbatchTemplate = Template(t.read())
-
-        tasks = []
-        indices = self.indices.split(",")
-
-        for product in products:
-            commandArgs = {
-                "jobWorkingDir": product["parentWorkDir"],
-                "inputMount": self.basketFolder,
-                "outputMount": self.outputFolder,
-                "stateMount": product["stateDir"],
-                "workingMount": product["workingDir"],
-                "tmpMount": product["tmpDir"],
-                "containerPath": self.containerPath,
-                "productId": product["productName"],
-                "indexCount": len(indices),
-                "indices": f"--indices {self.indices}",
-            }
-
-            sbatchScript = sbatchTemplate.substitute(commandArgs)
-            sbatchScriptPath = os.path.join(product["parentWorkDir"], self.sbatchFileName)
-
-            with open(sbatchScriptPath, "w") as sbatchFile:
-                sbatchFile.write(sbatchScript)
-
-            task = SubmitJob(
-                stateFolder=self.stateFolder,
-                name=product["productName"],
-                sbatchScriptPath=sbatchScriptPath,
-                testProcessing=self.testProcessing,
-            )
-
-            tasks.append(task)
-
-        yield tasks
-
-        output = {
-            "basket": self.basketFolder,
-            "jobs": []
-        }
-
-        for task in tasks:
-            submittedJob = {}
-            with task.output().open('r') as taskOutput:
-                submittedJob = json.load(taskOutput)
-
-            output["jobs"].append(submittedJob)
-
-        with self.output().open('w') as o:
-            json.dump(output, o, indent=4)
-
-    def output(self):
-        outFile = os.path.join(self.stateFolder, self.outfile)
-        return LocalTarget(outFile)
-
-
-@requires(SetupWorkDirs)
-class SubmitJobsForS1(SubmitJobs):
-    templateFilename = "s1_index_generation_job_template.sbatch"
-    platform = "S1"
-    sbatchFileName = "s1_index_generation_job.sbatch"
-    outfile = "SubmitJobsForS1.json"
-
-
-@requires(SetupWorkDirs)
-class SubmitJobsForS2(SubmitJobs):
-    templateFilename = "s2_index_generation_job_template.sbatch"
-    platform = "S2"
-    sbatchFileName = "s2_index_generation_job.sbatch"
-    outfile = "SubmitJobsForS2.json"
--- orchestration/__init__.py
+++ orchestration/__init__.py
@@ -1,4 +1,4 @@
 from .GetArdProducts import GetArdProducts
 from .SetupWorkDirs import SetupWorkDirs
 from .SubmitJob import SubmitJob
-from .SubmitJob import SubmitJobs
+from .SubmitJobs import SubmitJobs
