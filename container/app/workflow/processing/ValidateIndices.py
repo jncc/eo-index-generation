32eeefcd3385e1b18fc00195aa2e7b429ff8b997
Manvith Gayam <manvith.gayam@jncc.gov.uk>
2024-11-05 10:25:19 +0000

    Add qc check information to the statefile

--- container/app/workflow/processing/ValidateIndices.py
+++ container/app/workflow/processing/ValidateIndices.py
@@ -100,6 +100,8 @@ class ValidateIndices(luigi.Task):
         output = {
             "productId": self.productId,
             "qcPassed": True if processed_results else False,
+            "qcChecksPerformed": QcChecks.get_defaults(),
+            "perIndexRangeChecks": {k.value: v for k, v in QcChecks._range_checks.items()}
         }
 
         with self.output().open('w') as o:
