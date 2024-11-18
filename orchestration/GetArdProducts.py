b4e096e7296124efa565a7d85c31baf27e32f305
Manvith Gayam <manvith.gayam@jncc.gov.uk>
2024-11-18 17:53:18 +0000

    Allow workflow to process inputs.txt of products

--- orchestration/GetArdProducts.py
+++ orchestration/GetArdProducts.py
@@ -4,7 +4,9 @@ import os
 import re
 import logging
 from functional import seq
-from ceda_ard_finder import CreateSymlinks
+from datetime import datetime
+
+from ceda_ard_finder import CreateSymlinksFromFilters, CreateSymlinksFromTextFileList
 
 log = logging.getLogger('luigi-interface')
 
@@ -13,40 +15,21 @@ class GetArdProducts(luigi.Task):
     stateFolder = luigi.Parameter()
     basketFolder = luigi.Parameter()
 
-    # Ceda Ard Finder Params
-    startDate = luigi.DateParameter()
-    endDate = luigi.DateParameter()
-    ardFilter = luigi.Parameter()
-    spatialOperator = luigi.ChoiceParameter(choices=["", "intersects", "disjoint", "contains", "within"])
-    satelliteFilter = luigi.Parameter()
-
-    # Optional, rarely used Params
-    orbit = luigi.IntParameter(default=-9999)
-    orbitDirection = luigi.Parameter(default="")
-    wkt = luigi.Parameter(default="")
-
     def run(self):
-        task = CreateSymlinks(
-            stateFolder=self.stateFolder,
-            productLocation=self.basketFolder,
-            startDate=self.startDate,
-            endDate=self.endDate,
-            ardFilter=self.ardFilter,
-            spatialOperator=self.spatialOperator,
-            satelliteFilter=self.satelliteFilter,
-            orbit=self.orbit,
-            orbitDirection=self.orbitDirection,
-            wkt=self.wkt,
-        )
-        result = yield task
+
+        result = yield self.task()
 
         products = []
+
         with result.open("r") as createSymlinksFile:
             products = seq(json.load(createSymlinksFile)["products"]) \
                 .map(lambda x: os.path.basename(x)) \
                 .map(lambda x: re.sub(r"\.tif$", "", x)) \
                 .map(lambda x: re.sub(r"_vmsk_sharp_rad_srefdem_stdsref$", "", x)) \
                 .to_list()
+
+        if len(products) == 0:
+            raise ValueError("No Products Found")
 
         with self.output().open("w") as outFile:
             output = {
@@ -56,3 +39,40 @@ class GetArdProducts(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget(os.path.join(self.stateFolder, "GetArdProducts.json"))
+
+
+class GetArdProductsFromTextFileList(GetArdProducts):
+
+    def task(self):
+        return CreateSymlinksFromTextFileList(
+            stateFolder=self.stateFolder,
+            productLocation=self.basketFolder
+        )
+
+
+class GetArdProductsFromFilters(GetArdProducts):
+    # Ceda Ard Finder Params
+    startDate = luigi.Parameter()  # Date in YYYY-MM-DD format
+    endDate = luigi.Parameter()
+    ardFilter = luigi.Parameter()
+    spatialOperator = luigi.ChoiceParameter(choices=["", "intersects", "disjoint", "contains", "within"])
+    satelliteFilter = luigi.Parameter()
+
+    # Optional, rarely used Params
+    orbit = luigi.IntParameter(default=-9999)
+    orbitDirection = luigi.Parameter(default="")
+    wkt = luigi.Parameter(default="")
+
+    def task(self):
+        return CreateSymlinksFromFilters(
+            stateFolder=self.stateFolder,
+            productLocation=self.basketFolder,
+            startDate=datetime.strptime(self.startDate, "%Y-%m-%d"),  # Format str to datetime object
+            endDate=datetime.strptime(self.endDate, "%Y-%m-%d"),
+            ardFilter=self.ardFilter,
+            spatialOperator=self.spatialOperator,
+            satelliteFilter=self.satelliteFilter,
+            orbit=self.orbit,
+            orbitDirection=self.orbitDirection,
+            wkt=self.wkt
+        )
